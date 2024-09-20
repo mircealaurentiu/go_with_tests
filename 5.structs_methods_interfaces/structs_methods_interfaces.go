b96e5330ee6242deb6c526f7c606a33f3b3dc15c
Mircea <laurentiu.m.tudorascu@gmail.com>
2024-09-20 23:09:41 +0300

    added methods for rectangle, circle

--- 5.structs_methods_interfaces/structs_methods_interfaces.go
+++ 5.structs_methods_interfaces/structs_methods_interfaces.go
@@ -1,8 +1,22 @@
 package structsmethodsinterfaces
 
+import "math"
+
 type Rectange struct {
 	Width  float64
 	Height float64
+}
+
+func (r Rectange) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectange) float64 {
--- 5.structs_methods_interfaces/structs_methods_interfaces_test.go
+++ 5.structs_methods_interfaces/structs_methods_interfaces_test.go
@@ -13,11 +13,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectange := Rectange{6.0, 7.0}
-	got := Area(rectange)
-	want := 42.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectange{6.0, 7.0}
+		got := rectangle.Area()
+		want := 42.0
 
-	if got != want {
-		t.Errorf("\ngot:  %.2f \nwant: %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("\ngot:  %.2f \nwant: %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("\ngot:  %g \nwant: %g", got, want)
+		}
+
+	})
 }
