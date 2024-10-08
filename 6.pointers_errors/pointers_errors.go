fec036adecd1cc56ba5e169b534e7cf5346c602a
Mircea <tudorascu@pm.me>
2024-10-08 16:19:24 +0300

    added 6.pointer_errors folder

--- 6.pointers_errors/pointers_errors.go
+++ 6.pointers_errors/pointers_errors.go
@@ -1,11 +1,38 @@
 package pointerserrors
 
-type Wallet struct{}
+import (
+	"errors"
+	"fmt"
+)
 
-func (w Wallet) Deposit(amount int) {
+type Bitcoin int
 
+type Wallet struct {
+	balance Bitcoin
 }
 
-func (w Wallet) Balance() int {
-	return 0
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
