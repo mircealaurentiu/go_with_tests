61a9ad85656a9e6b4da8b46876e313bcf76a4eb4
neptune <tudorascu@pm.me>
2024-11-12 16:00:50 +0200

    updated NewPostsFromFS, getting Title works

--- 17.reading_files/blogposts/blogposts.go
+++ 17.reading_files/blogposts/blogposts.go
@@ -1,10 +1,56 @@
 package blogposts
 
-import "testing/fstest"
+import (
+	"errors"
+	"io"
+	"io/fs"
+)
 
 type Post struct {
+	Title string
 }
 
-func NewPostsFromFS(filesystem fstest.MapFS) []Post {
-	return nil
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("fail")
+}
+
+func NewPostsFromFS(filesystem fs.FS) ([]Post, error) {
+
+	dir, err := fs.ReadDir(filesystem, ".")
+	if err != nil {
+		return nil, err
+	}
+
+	var posts []Post
+
+	for _, f := range dir {
+		post, err := getPost(filesystem, f)
+		if err != nil {
+			return nil, err // needs clarif, should we fail if one fails?
+		}
+		posts = append(posts, post)
+	}
+
+	return posts, nil
+}
+
+func getPost(filesystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := filesystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+
+	defer postFile.Close()
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+
+	return post, nil
 }
--- 17.reading_files/blogposts/blogposts_test.go
+++ 17.reading_files/blogposts/blogposts_test.go
@@ -1,6 +1,7 @@
 package blogposts_test
 
 import (
+	"reflect"
 	"testing"
 	"testing/fstest"
 
@@ -9,13 +10,24 @@ import (
 
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello_world.md":   {Data: []byte("hi")},
-		"hello_world_2.md": {Data: []byte("hola")},
+		"hello_world.md":   {Data: []byte("Title: Post 1")},
+		"hello_world_2.md": {Data: []byte("Title: Post 2")},
 	}
 
-	posts := blogposts.NewPostsFromFS(fs)
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if len(posts) != len(fs) {
 		t.Errorf("\ngot  %d posts \nwant %d posts", len(posts), len(fs))
+	}
+
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("\ngot  %+v, want %+v", got, want)
 	}
 }
