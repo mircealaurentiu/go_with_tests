fa680fcc82c80fbc9b8ebfeb18896ab692797201
neptune <tudorascu@pm.me>
2024-11-19 21:00:02 +0200

    added test and implementation for title in h1 tag

---
+++ 18.templating/templating_test.go
@@ -0,0 +1,35 @@
+package blogrender_test
+
+import (
+	"bytes"
+	"testing"
+
+	blogrender "github.com/mircealaurentiu/go_with_tests/18.templating"
+)
+
+func TestRender(t *testing.T) {
+
+	aPost := blogrender.Post{
+		Title:       "hello world",
+		Body:        "this is a post",
+		Description: "this is a description",
+		Tags:        []string{"go", "tdd"},
+	}
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := blogrender.Render(&buf, aPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := "<h1>hello world</h1>"
+
+		if got != want {
+			t.Errorf("\ngot  %s \nwant %s", got, want)
+		}
+	})
+
+}
