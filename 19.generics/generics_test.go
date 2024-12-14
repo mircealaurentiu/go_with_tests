f7fdc306f4da1cb608416143fd6072f62ac4a843
neptune <tudorascu@pm.me>
2024-12-14 23:07:43 +0200

    finished generics chapter

--- 19.generics/generics_test.go
+++ 19.generics/generics_test.go
@@ -14,9 +14,78 @@ func TestAssertFunction(t *testing.T) {
 		AssertNotEqual(t, "hello", "notHello")
 	})
 
+	//AssertEqual(t, 1, "1") // this does not work, it is supposed to not work
+
 }
 
-func AssertEqual(t *testing.T, got, want interface{}) {
+func TestStack(t *testing.T) {
+	t.Run("integers stack", func(t *testing.T) {
+
+		myStackOfInt := new(StackOfInts)
+
+		//check if stack is empty
+		AssertTrue(t, myStackOfInt.IsEmpty())
+
+		// add item, then check it is not empty
+		myStackOfInt.Push(11)
+		AssertFalse(t, myStackOfInt.IsEmpty())
+
+		// add another item, then pop it
+		myStackOfInt.Push(22)
+		popValue, _ := myStackOfInt.Pop()
+		AssertEqual(t, popValue, 22)
+
+		popValue, _ = myStackOfInt.Pop()
+		AssertEqual(t, popValue, 11)
+
+		AssertTrue(t, myStackOfInt.IsEmpty())
+	})
+
+	t.Run("string stack", func(t *testing.T) {
+
+		myStackOfInt := new(StackOfStrings)
+
+		//check if stack is empty
+		AssertTrue(t, myStackOfInt.IsEmpty())
+
+		// add item, then check it is not empty
+		myStackOfInt.Push("11")
+		AssertFalse(t, myStackOfInt.IsEmpty())
+
+		// add another item, then pop it
+		myStackOfInt.Push("22")
+		popValue, _ := myStackOfInt.Pop()
+		AssertEqual(t, popValue, "22")
+
+		popValue, _ = myStackOfInt.Pop()
+		AssertEqual(t, popValue, "11")
+
+		AssertTrue(t, myStackOfInt.IsEmpty())
+	})
+
+	t.Run("integer stack with generics", func(t *testing.T) {
+		myStackOfInt := new(Stack[int])
+
+		//check if stack is empty
+		AssertTrue(t, myStackOfInt.IsEmpty())
+
+		// add item, then check it is not empty
+		myStackOfInt.Push(11)
+		AssertFalse(t, myStackOfInt.IsEmpty())
+
+		// add another item, then pop it
+		myStackOfInt.Push(22)
+		popValue, _ := myStackOfInt.Pop()
+		AssertEqual(t, popValue, 22)
+
+		popValue, _ = myStackOfInt.Pop()
+		AssertEqual(t, popValue, 11)
+
+		AssertTrue(t, myStackOfInt.IsEmpty())
+	})
+}
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
 
 	t.Helper()
 	if got != want {
@@ -25,11 +94,25 @@ func AssertEqual(t *testing.T, got, want interface{}) {
 
 }
 
-func AssertNotEqual(t *testing.T, got, want interface{}) {
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 
 	t.Helper()
 	if got == want {
 		t.Errorf("\ngot  %+v \nwant %+v", got, want)
 	}
 
+}
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("\ngot  %v \nwant true", got)
+	}
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("\ngot  %v \nwant false", got)
+	}
 }
